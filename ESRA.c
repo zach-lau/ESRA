52926104c7b3016c25f9b340f8e7e643a58c2cb2
Zachary Lau <zlau2000@gmail.com>
2019-03-12 15:58:49 -0600

    The architecture will be copmlete shortly

---
+++ ESRA.c
@@ -0,0 +1,21 @@
+/*
+The main architecture for the ESRA
+*/
+
+#include "simpletools.h"
+#include "servo.h"
+
+#include "angry.h"
+//#include "excited.h"
+//#include "scared.h"
+//#include "confused.h"
+
+enum emotion{ANGRY, EXCITED, SCARED, CONFUSED};
+
+
+emotion state = EXCITED;
+
+int main(){ 
+  
+  
+}
